88b244516a5fda9546e6fb8b612385e1c2375881
tairoroberto <tairoroberto@hotmail.com>
2017-10-18 22:31:18 -0200

    initial commit

---
+++ XF.Hello/XF.Hello/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.Hello
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
